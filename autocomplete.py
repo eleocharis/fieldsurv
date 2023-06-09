392113955b645f345513a0d0fe10acd17b7ba05a
eleocharis <jan-h@posteo.de>
2023-06-09 11:08:26 +0200

    Add comments. Change the AutoComplet classes inheritance to a widget not a screen. that make it inheritable for other screen classes. A second timescadule for the tf is add for slo machines

--- autocomplete.py
+++ autocomplete.py
@@ -1,12 +1,6 @@
-'''
-This module creates inline autocompletion for species filtered from species lists.
-It needs a TextInput or MDTextfield with the id: tf and
-a (MD)StackLayout (or an alternative layout but stack works best) will handel the suggestion buttons
-see autocomplete.kv
-'''
-
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.clock import Clock
 from kivymd.uix.button import MDFillRoundFlatButton
@@ -16,15 +10,21 @@ import pandas as pd
 Builder.load_file('autocomplete.kv')
 
 
-class AutoComplete(Screen):
+class AutoComplete(Widget):
+    '''
+    This module creates inline autocompletion for species filtered from species lists.
+    It needs a TextInput or MDTextfield with the id: tf and
+    a (MD)StackLayout (or an alternative layout but stack works best) will handel the suggestion buttons
+    see autocomplete.kv
+    '''
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.species_dict = defaultdict(list)
         self.prepare_species_list()
 
     def prepare_species_list(self):
-        # prepares the species lists into dictionaries, to filter for genus
-        # and after only the species within the selected genus.
+        ''' prepares the species lists into dictionaries, to filter for genus
+        and after only the species within the selected genus.'''
         file = str('data/species_lists/spec_germany_Pflanzen.csv')
         species_df = pd.read_csv(file)
         for index, row in species_df.iterrows():
@@ -51,19 +51,20 @@ class AutoComplete(Screen):
         # create suggestion buttons in the MDStackLayout with id 'word suggest':
         # by pressing the button the "paste" method is applied
         self.ids.word_suggests.clear_widgets()
-        for suggestion in filtered_suggestions[:20]:
+        for suggestion in filtered_suggestions[:10]:
             button = MDFillRoundFlatButton(text=suggestion, id=suggestion, on_press=self.paste)
             self.ids.word_suggests.add_widget(button)
 
     def paste(self, button):
-        # Bring the button text (which is the previously generated suggestion to the textfield).
-        # f-string is created from the first word (mostly genus) if present, and the text from the button.
-        # to prevent that already typed words and letters are not pasted two times, length of this text string
-        # is measured and subtracted from the button.text string. It cannot just be set.
+        ''' Bring the button text (which is the previously generated suggestion to the textfield).
+        f-string is created from the first word (mostly genus) if present, and the text from the button.
+        to prevent that already typed words and letters are not pasted two times, length of this text string
+        is measured and subtracted from the button.text string. It cannot just be set. '''
         self.ids.tf.text = f'{self.ids.tf.text}{button.text[len(self.ids.tf.text.split()[-1]):]} '.lstrip()
-        self.ids.tf.text = self.ids.tf.text[0].upper() +  self.ids.tf.text[1:]# Bug: should upper first word but not lower other words
+        self.ids.tf.text = self.ids.tf.text[0].upper() + self.ids.tf.text[1:]
         # set_focus back to the text field does not work alone. it has to be scheduled.
-        Clock.schedule_once(self.set_focus, 0.1)
+        Clock.schedule_once(self.set_focus, 0.2)
+        Clock.schedule_once(self.set_focus, 0.6)  # to make shure that it is palced also on slow machines.
 
     def set_focus(self, event):
         self.ids.tf.focus = True
