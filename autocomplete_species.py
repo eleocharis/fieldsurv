6391bc87929423a22c28a12edb6aa5f2b118003d
eleocharis <jan-h@posteo.de>
2023-06-11 17:12:00 +0200

    remove the prepare list function. it is moved to usersettings.py

--- autocomplete_species.py
+++ autocomplete_species.py
@@ -1,12 +1,11 @@
 from kivymd.app import MDApp
 from kivy.uix.widget import Widget
-from kivy.lang import Builder
+# from kivy.lang import Builder
 from kivy.clock import Clock
 from kivymd.uix.button import MDFillRoundFlatButton
-from collections import defaultdict
-import pandas as pd
+from usersettings import SPEC_AUT_C_DICT
 
-Builder.load_file('autocomplete.kv')
+# Builder.load_file('autocomplete.kv')
 
 
 class AutoCompleteSp(Widget):
@@ -18,18 +17,6 @@ class AutoCompleteSp(Widget):
     '''
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.species_dict = defaultdict(list)
-        self.prepare_species_list()
-
-    def prepare_species_list(self):
-        ''' prepares the species lists into dictionaries, to filter for genus
-        and after only the species within the selected genus.'''
-        file = str('data/species_lists/spec_germany_Pflanzen.csv')
-        species_df = pd.read_csv(file)
-        for index, row in species_df.iterrows():
-            sp = row["sciName"]
-            genus, species = sp.split(" ", maxsplit=1)
-            self.species_dict[genus].append(species)
 
     def create_suggestions(self, instance, value):
         tf_input = value.split()  # Split tf_input text into separate words
@@ -41,10 +28,10 @@ class AutoCompleteSp(Widget):
 
         elif len(tf_input) == 1:
             # Check if first word matches the beginning of any suggestion of e.g. genus
-            filtered_suggestions = [key for key in self.species_dict.keys()
+            filtered_suggestions = [key for key in SPEC_AUT_C_DICT.keys()
                                     if key.lower().startswith(tf_input[0].lower())]
         else:
-            filtered_suggestions = [key for key in self.species_dict[tf_input[0]]
+            filtered_suggestions = [key for key in SPEC_AUT_C_DICT[tf_input[0]]
                                     if key.startswith(tf_input[1])]
 
         # create suggestion buttons in the MDStackLayout with id 'word suggest':
@@ -58,12 +45,12 @@ class AutoCompleteSp(Widget):
         ''' Bring the button text (which is the previously generated suggestion to the textfield).
         f-string is created from the first word (mostly genus) if present, and the text from the button.
         to prevent that already typed words and letters are not pasted two times, length of this text string
-        is measured and subtracted from the button.text string. It cannot just be set. '''
+        is measured and subtracted from the "button.text" string. It cannot just be set. '''
         self.ids.tf.text = f'{self.ids.tf.text}{button.text[len(self.ids.tf.text.split()[-1]):]} '.lstrip()
         self.ids.tf.text = self.ids.tf.text[0].upper() + self.ids.tf.text[1:]
         # set_focus back to the text field does not work alone. it has to be scheduled.
         Clock.schedule_once(self.set_focus, 0.2)
-        Clock.schedule_once(self.set_focus, 0.6)  # to make shure that it is palced also on slow machines.
+        Clock.schedule_once(self.set_focus, 0.6)  # to make sure that it is placed also on slow machines.
 
     def set_focus(self, event):
         self.ids.tf.focus = True
@@ -77,6 +64,6 @@ if __name__ == '__main__':
             self.theme_cls.primary_hue = '700'
             self.theme_cls.accent_palette = 'Orange'
 
-            autocomp = AutoCompleteSp()
-            return autocomp
+            auto_comp = AutoCompleteSp()
+            return auto_comp
     MainApp().run()
