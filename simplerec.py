3318136938a36cb667088747c7060823b1a4dc9a
eleocharis <jan-h@posteo.de>
2023-06-19 22:55:03 +0200

    Add methods to the pointcreator to show point information. add a delete method, which is not jet working. Add ID to the records to set point IDs. Create 3ListItems to list all records. It replaces the slow and buggy table.

--- simplerec.py
+++ simplerec.py
@@ -4,13 +4,13 @@ from kivy.core.window import Window
 from kivy_garden.mapview import MapMarkerPopup, MapSource
 from kivy.graphics import Color, Line
 from kivy.clock import Clock
-from kivy.metrics import dp
-from kivy.uix.button import Button
 from kivymd.uix.pickers import MDDatePicker, MDTimePicker
-from kivymd.uix.datatables import MDDataTable
 from kivymd.uix.button import MDFloatingActionButtonSpeedDial
 from kivymd.uix.menu import MDDropdownMenu
-from kivymd.uix.list import MDList
+from kivymd.uix.button import MDFillRoundFlatButton
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.label import MDLabel
+from kivymd.uix.list import ThreeLineIconListItem, IconLeftWidget
 from kivy.animation import Animation
 from pathlib import Path
 from autocomplete_species import AutoCompleteSp
@@ -30,22 +30,51 @@ class SpeedDialButton(MDFloatingActionButtonSpeedDial):
             'C++': 'language-cpp',
         }
 
+
 class PointCreator(MapMarkerPopup):
-    def __init__(self, x, y, **kwargs):
+    def __init__(self, x, y, spec, abu, date, id, **kwargs):
         super().__init__(**kwargs)
         self.x = x
         self.y = y
+        self.species = spec
+        self.abundance = abu
+        self.date = date
+        self.id = id
+
+    def popup_info(self):
+        layout = MDBoxLayout(size_hint=(None, None), size=[200, 100], orientation='vertical', md_bg_color=[1, 1, 1, .8])
+        label = MDLabel(text=f'{self.species}\nn: {self.abundance}\n{self.date}', theme_text_color="Custom",
+                        text_color=[0, 0, 5, 1])
+        layout.add_widget(label)
+        button = MDFillRoundFlatButton(text="Delete Point?", on_release=self.delete_point)
+        layout.add_widget(button)
+        self.add_widget(layout)
+        #print(self.ids.items())
+        #print(self.id)
+
+    def delete_point(self, button):
+        print(button.parent.parent.parent)
+        #sr = SimpleRec()
+        self.remove_widget(button.parent.parent.parent)
+
 
 
 class SimpleRec(MDScreen, AutoCompleteSp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.file = 'data/simple_point_records.csv'
+        self.records = None
+        self.next_id = None
+        self.points = {}
         self.read_point_records_file()
-        # self.fill_records_table()
         self.ids.date.text = datetime.date.today().strftime("%Y-%m-%d")
         self.ids.time.text = datetime.datetime.now().strftime("%H:%M")
         Clock.schedule_interval(lambda dt: self.crosshair(), 1)
+
+        # add alternative map sources to the MapSource
+        alternative_map_source = (0, 0, 19, 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+                        'Kartendaten: © OpenStreetMap-Mitwirkende, SRTM | Kartendarstellung: © OpenTopoMap (CC-BY-SA)')
+        MapSource.providers["opentopomap"] = alternative_map_source
 
         # Dropdown menu for maps.
         maps = [
@@ -58,7 +87,8 @@ class SimpleRec(MDScreen, AutoCompleteSp):
             caller=self.ids.set_map_button,
             items=maps,
             width_mult=3,
-            elevation=0)
+            elevation=0,
+            background_color=[1, 1, 1, .8])
 
     def crosshair(self):
         x, y = self.ids.map.center
@@ -69,9 +99,9 @@ class SimpleRec(MDScreen, AutoCompleteSp):
             Line(points=[x - self.width*0.03, y, x + self.width*0.03, y], width=1, group=u"cross")
 
     def add_points(self):
-        # print(f'{self.ids.map.lat} | {self.ids.map.lon}')
-        # add points to the records DataFrame
-        point_attributes = {"species": self.ids.tf.text,
+        # Add points to the records DataFrame
+        point_attributes = {"id": self.next_id,
+                            "species": self.ids.tf.text,
                             "abundance": self.ids.abundance.text,
                             "timestamp": f'{self.ids.date.text} {self.ids.time.text}',
                             "lat": self.ids.map.lat,
@@ -79,54 +109,47 @@ class SimpleRec(MDScreen, AutoCompleteSp):
         self.records.loc[len(self.records.index)] = point_attributes
         print(self.records)
 
+        # Add points to the Table
+        self.fill_records_table(point_attributes)
+
         # Set the points on the map
         x = self.ids.map.lat
         y = self.ids.map.lon
-        point = PointCreator(lat=self.ids.map.lat, lon=self.ids.map.lon, x=x, y=y)
-        point_information = Button(text=str(x + y))  # Does not work jet
-        point.add_widget(point_information)
+        point = PointCreator(lat=self.ids.map.lat, lon=self.ids.map.lon, x=x, y=y, spec=self.ids.tf.text,
+                             abu=self.ids.abundance, date=f'{self.ids.date.text} {self.ids.time.text}', id=self.next_id)
+        self.points[self.next_id] = point #store all points in a dict.
         self.ids.map.add_widget(point)
 
-        # empty fields and set focus
+        # Generate next id
+        self.next_id = str("F" + str(int(self.records.iloc[-1]["id"][1:]) + 1))
+
+        # Empty fields and set focus
         self.ids.tf.text = ""
         self.ids.abundance.text = "1"
         self.ids.time.text = datetime.datetime.now().strftime("%H:%M")
         self.ids.tf.focus = True
 
-        # add points to the Table
-        # print(list(self.records.iloc[-1]))
-        # Clock.schedule_once(lambda dt: self.table.add_row(list(self.records.iloc[-1])))  #
-
     def read_point_records_file(self):
-        # reads previously recorded and saved species data from the drive
+        # reads recorded data from the previous session and creates the points
         if Path(self.file).is_file():
             self.records = pd.read_csv(self.file)
+
             for index, row in self.records.iterrows():
                 lat, lon = float(row["lat"]), float(row["lon"])
-                point = PointCreator(lat=lat, lon=lon, x=lat, y=lon)
-                point_information = Button(size=(self.width*0.2, self.height*0.1),
-                                           text=f'{row["species"]} {row["abundance"]}') # Does not work jet
-                point.add_widget(point_information)
+                point = PointCreator(lat=lat, lon=lon, x=lat, y=lon, spec=row["species"], abu=row["abundance"],
+                                     date=row["timestamp"], id=row["id"])
+                self.points[row["id"]] = point  # store all points in a dict.
                 self.ids.map.add_widget(point)
+
+                # add the entry to the Table
+                self.fill_records_table(row)
+
+            # create next ID
+            self.next_id = str("F" + str(int(self.records.iloc[-1]["id"][1:]) + 1))
+
         else:
             self.records = pd.DataFrame(columns=["species", "abundance", "timestamp", "lat", "lon"])
-
-    def fill_records_table(self):
-        records_for_table = self.records
-        records_for_table["lat"] = records_for_table["lat"].round(decimals=4)
-        records_for_table["lon"] = records_for_table["lon"].round(decimals=4)
-        records_for_table = records_for_table.values.tolist()
-        headers_for_table = list(self.records.columns)
-        self.table = MDDataTable(
-            column_data = [(header, dp(20)) for header in headers_for_table],
-            row_data = records_for_table,
-            rows_num = len(records_for_table),
-            #check = True,
-            elevation = 0,
-            background_color = [1, 1, 1, .8]
-        )
-        self.ids.record_table.add_widget(self.table)
-        self.table.bind(on_row_press=self.on_row_press)
+            self.next_id = "F1"
 
     def save_records(self):
         # this saves the recordings on the drive
@@ -175,35 +198,43 @@ class SimpleRec(MDScreen, AutoCompleteSp):
             move_button.start(show_input_button)
             self.ids.show_input_button.icon = 'plus'
 
-    def show_record_table(self, record_table, show_records_button):
+    def set_map_source(self, selected_map):
+        self.ids.map.map_source = selected_map
+        self.map_dropdown.dismiss()
+        print(MapSource.providers.values())
+
+
+    def fill_records_table(self, last_entry):
+        self.ids.record_table.add_widget(
+            ThreeLineIconListItem(
+                IconLeftWidget(icon='flower'),
+                text=str(last_entry["species"]),
+                secondary_text=str("Abundance " + str(last_entry["abundance"])),
+                tertiary_text=str(last_entry["timestamp"]),
+                on_release=lambda x: self.click_table_item()
+            )
+        )
+
+    def click_table_item(self):
+        print("Item clicked!")
+
+    def show_record_table(self, record_table_layout, show_records_button):
         # moves the record list into the screen
         if self.ids.show_records_button.icon == 'view-headline':
-            show_records = Animation(pos=[0, Window.size[1]-self.ids.record_table.height], duration=0.2)
-            show_records.start(record_table)
+            show_records = Animation(pos=[0, Window.size[1]-self.ids.record_table_layout.height], duration=0.2)
+            show_records.start(record_table_layout)
 
             move_button = Animation(
-                pos_hint={'center_x': 0.1, "center_y": (1 - self.ids.record_table.height / Window.size[1]) * 0.95},
+                pos_hint={'center_x': 0.1, "center_y": (1 - self.ids.record_table_layout.height / Window.size[1]) * 0.92},
                 duration=0.2)
             move_button.start(show_records_button)
             self.ids.show_records_button.icon = 'chevron-up'
         else:
             show_records = Animation(pos=[0, Window.size[1]], duration=0.2)
-            show_records.start(record_table)
+            show_records.start(record_table_layout)
 
             move_button = Animation(
                 pos_hint={'center_x': 0.1, "center_y": 0.94},
                 duration=0.2)
             move_button.start(show_records_button)
             self.ids.show_records_button.icon = 'view-headline'
-
-    def on_row_press(self, instance_table, instance_row):
-        print(f'{instance_table} | {instance_row}')
-
-    def set_map_source(self, selected_map):
-        self.ids.map.map_source = selected_map
-        self.map_dropdown.dismiss()
-        #print(MapSource.providers.get(selected_map))
-
-
-
-
