02270c715078aee3b84aa9fb68023a436217a629
eleocharis <jan-h@posteo.de>
2023-07-12 08:26:32 +0200

    minor changes

--- logfile_redirect.py
+++ logfile_redirect.py
@@ -3,7 +3,7 @@ from os.path import join, exists, expanduser
 from os import makedirs
 
 """ Sets the path to the log file. """
-log_path = join(expanduser("~"), 'prgwerk/buidozer/logs')
+log_path = join(expanduser("~"), 'prgwerk/buildozer/logs')
 if not exists(log_path):
     makedirs(log_path)
 Config.set('kivy', 'log_dir', log_path)
